935698d6ef3001d7a2d2641caa5a2ab950186020
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-02-22 16:28:50 +0100

    Allow using downloader.download
    Needed for genericIE and playlists

--- main.py
+++ main.py
@@ -16,19 +16,41 @@ class SimpleFileDownloader(youtube_dl.FileDownloader):
     def __init__(self,*args,**kargs):
         super(SimpleFileDownloader,self).__init__(*args,**kargs)
         self._screen_file=NoneFile()
-    pass
+        self.videos_info=[]
+        self.ie=None
+    def _addVideos(self,vids):
+        for vid in vids:
+            self.videos_info.append(vid)
+    def _addIE(self,ie):
+        if not self.ie:
+            self.ie = ie
+    def _extract(self,url):
+        ies = youtube_dl.gen_extractors()
+        for ie in ies:
+            ie.set_downloader(self)
+            if ie.suitable(url):
+                vids = ie.extract(url)
+                return vids,ie
+    def download(self, url_list):
+        '''Hack, some IE call it for downloading video urls'''
+        for url in url_list:
+            vids,ie = self._extract(url)
+            if vids:
+                self._addVideos(vids)
+
+    def extract(self,url):
+        videos_info,ie = self._extract(url)
+        if videos_info:
+            self._addVideos(videos_info)
+            self._addIE(ie)
+        return self.videos_info,self.ie
 
 def videos(url):
     '''
     Get a list with a dict for every video founded
     '''
-    ies = youtube_dl.gen_extractors()[:-1] #We ignore GenericIE
     fd = SimpleFileDownloader({'outtmpl':'%(title)s'})
-    for ie in ies:
-        ie.set_downloader(fd)
-        if ie.suitable(url):
-            vids = ie.extract(url)
-            return vids,ie
+    return fd.extract(url)
 
 class Api(webapp2.RequestHandler):
     def get(self):
