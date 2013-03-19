ba65124696a47488b9be1a72543877183b3730b7
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-03-19 14:51:47 +0100

    Add command line option for the port the server uses

--- youtube_dl_server.py
+++ youtube_dl_server.py
@@ -1,4 +1,5 @@
 from paste import httpserver
+import argparse
 
 from app import app
 
@@ -7,7 +8,20 @@ from app import app
 """
 
 def main():
-    httpserver.serve(app, host='localhost', port='9191')
+
+    desc="""
+         The youtube-dl API server.
+         """
+    default_port = 9191
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-p','--port',
+                       default= default_port,
+                       type=int,
+                       help='The port the server will use. The default is: {}'.format(default_port)
+                       )
+    
+    args = parser.parse_args()
+    httpserver.serve(app, host='localhost', port=args.port)
 
 if __name__ == '__main__':
     main()
