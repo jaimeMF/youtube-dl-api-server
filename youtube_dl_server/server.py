5a7f34323ce4db192f588cab503dab4f21bcb0bf
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-05-24 18:20:14 +0200

    Add version option to the command line arguments

--- youtube_dl_server/server.py
+++ youtube_dl_server/server.py
@@ -2,6 +2,7 @@ from paste import httpserver
 import argparse
 
 from .app import app
+from .version import __version__
 
 """
     A server for providing the app anywhere, no need for GAE
@@ -19,8 +20,12 @@ def main():
                        type=int,
                        help='The port the server will use. The default is: {}'.format(default_port)
                        )
-    
+    parser.add_argument('--version', action='store_true', help='Print the version of the server')
+
     args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        exit(0)
     httpserver.serve(app, host='localhost', port=args.port)
 
 if __name__ == '__main__':
