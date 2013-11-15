f088ec4557da5d198c68e8b4059a83ed6e043826
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-11-15 14:03:26 +0100

    Remove the import of the fcntl module (only for GAE)

--- devscripts/setup-gae.sh
+++ devscripts/setup-gae.sh
@@ -11,3 +11,13 @@ echo "Copying python packages to ${LIB_DIR}"
 mkdir -p "${LIB_DIR}"
 cp -R "${BUILD_DIR}/Paste/paste" "${LIB_DIR}"
 cp -R "${BUILD_DIR}/youtube-dl/youtube_dl" "${LIB_DIR}"
+
+(
+    cd "${LIB_DIR}/youtube_dl"
+    echo 'Patching youtube_dl'
+    sed 's/import fcntl/# Removed fcntl import/g' utils.py > utils.py.temp
+    mv utils.py.temp utils.py
+)
+
+rm -rf "${BUILD_DIR}"
+
