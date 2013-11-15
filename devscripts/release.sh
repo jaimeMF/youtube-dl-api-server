bf95476f19d36273ff14579bbbb66dd1b258638c
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-11-15 14:16:52 +0100

    Update release.sh

--- devscripts/release.sh
+++ devscripts/release.sh
@@ -2,14 +2,21 @@
 
 set -e
 
-version=`python -m youtube_dl_server --version`
-echo "New version is $version"
+if [ -z "$1" ]; then echo "ERROR: specify version like this: $0 0.9.10"; exit 1; fi
+version=$1
+gae_version=$(python -c "print('$version'.replace('.', '-'))")
+echo "New version is $version (for GAE: $gae_version)"
 
 read -p "Is it good, can I continue? (y/n) " -n 1
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then exit 1; fi
 echo
 
-# TODO: create a GIT tag
+echo "__version__ = '$version'" > youtube_dl_server/version.py
+sed -i '' "s/^version: .*/version: $gae_version/" app.yaml
+
+git add youtube_dl_server/version.py app.yaml
+git commit -m "Release $version"
+git tag $version
 
 echo "Uploading to PyPI"
 python setup.py sdist upload --sign
