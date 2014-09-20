9c45fd5821252ee0616e3d0336ee839bdc23c7d7
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2014-09-20 11:28:46 +0200

    devscripts/release.sh: Edit and commit CHANGELOG.rst

--- devscripts/release.sh
+++ devscripts/release.sh
@@ -15,7 +15,9 @@ echo "__version__ = '$version'" > youtube_dl_server/version.py
 sed -i '' "s/^version: .*/version: $gae_version/" app.yaml
 sed -i '' "s/version='.*'/version='$version'/" setup.py
 
-git add youtube_dl_server/version.py app.yaml setup.py
+$EDITOR CHANGELOG.rst
+
+git add youtube_dl_server/version.py app.yaml setup.py CHANGELOG.rst
 git commit -m "Release $version"
 git tag $version
 
