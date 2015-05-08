cf53dcff08826f1d79ad629afe8ef376cd92f1e1
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-05-08 13:26:33 +0200

    travis: Use tox for running the tests

---
+++ devscripts/travis-tox-env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Convert the Travis python version to a tox environment
+TRAVIS_ENV=$1
+
+case $TRAVIS_ENV in
+    pypy*) echo $TRAVIS_ENV;;
+    *) echo py$TRAVIS_ENV | sed 's/\.//g';;
+esac
